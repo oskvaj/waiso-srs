9fc0fb5e78cc6fd573702b4cdf2fd07746cb1f56
oskvaj <oskarvajlok@gmail.com>
2026-04-10 11:00:29 +0200

    feat: added UpploadThing Token to env

--- src/env.js
+++ src/env.js
@@ -21,6 +21,7 @@ export const env = createEnv({
     EMAIL_SERVER_USER: z.string(),
     EMAIL_SERVER_PASSWORD: z.string(),
     EMAIL_FROM: z.string().email(),
+    UPLOADTHING_TOKEN: z.string(),
   },
 
   /**
@@ -46,6 +47,8 @@ export const env = createEnv({
     EMAIL_SERVER_USER: process.env.EMAIL_SERVER_USER,
     EMAIL_SERVER_PASSWORD: process.env.EMAIL_SERVER_PASSWORD,
     EMAIL_FROM: process.env.EMAIL_FROM,
+
+    UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
