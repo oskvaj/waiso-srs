0be6163c9bb83f125dbb950a2756f5d2738345d9
kevinlun-git <kevin.lundkvist1@gmail.com>
2026-04-17 09:59:06 +0200

    feat: use teacherProcedure, add create
    Co-authored-by: oskvaj <oskarvajlok@gmail.com>

--- src/server/api/routers/course.ts
+++ src/server/api/routers/course.ts
@@ -1,8 +1,18 @@
-import { listCoursesForTeacher } from "@/server/services/course";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import {
+  createCourse,
+  createCourseSchema,
+  listCoursesForTeacher,
+} from "@/server/services/course";
+import { createTRPCRouter, teacherProcedure } from "../trpc";
 
 export const courseRouter = createTRPCRouter({
-  listMine: protectedProcedure.query(({ ctx }) => {
+  listMine: teacherProcedure.query(({ ctx }) => {
     return listCoursesForTeacher(ctx.db, ctx.session.user.id);
   }),
+
+  create: teacherProcedure
+    .input(createCourseSchema)
+    .mutation(({ ctx, input }) => {
+      return createCourse(ctx.db, ctx.session.user.id, input);
+    }),
 });
