29ad97eb3790be2af390bab22942bc737666bc79
kevinlun-git <kevin.lundkvist1@gmail.com>
2026-04-18 14:54:01 +0200

    feat: getOverview teacherProcedure
    Co-authored-by: oskvaj <oskarvajlok@gmail.com>

--- src/server/api/routers/course.ts
+++ src/server/api/routers/course.ts
@@ -1,9 +1,11 @@
 import {
   createCourse,
   createCourseSchema,
+  getCourseOverview,
   listCoursesForTeacher,
 } from "@/server/services/course";
 import { createTRPCRouter, teacherProcedure } from "../trpc";
+import z from "zod";
 
 export const courseRouter = createTRPCRouter({
   listMine: teacherProcedure.query(({ ctx }) => {
@@ -14,5 +16,11 @@ export const courseRouter = createTRPCRouter({
     .input(createCourseSchema)
     .mutation(({ ctx, input }) => {
       return createCourse(ctx.db, ctx.teacher.userId, input);
+    }),
+
+  getOverview: teacherProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return getCourseOverview(ctx.db, input.id, ctx.teacher.userId);
     }),
 });
