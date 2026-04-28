1996aff387f2c5a5901d330b4a264a6cd0b9e382
oskvaj <oskarvajlok@gmail.com>
2026-04-28 09:19:27 +0200

    feat: Added a link to student course overview
    Co-authored-by: kevinlun-git <Kevin.lundkvist1@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,10 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <div className="text-4xl">
-            <Link href="/courses">Courses Link</Link>
+            <Link href="/courses">Teacher Link</Link>
+          </div>
+          <div className="text-4xl">
+            <Link href="/course">Student Link</Link>
           </div>
           <div>{session ? "You are signed in" : "You are not signed in"}</div>
           <div className="flex flex-col items-center gap-2">
