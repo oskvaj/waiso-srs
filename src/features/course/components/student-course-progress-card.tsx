4a4b820a7904bea6089f526abc7083dbcc57039d
oskvaj <oskarvajlok@gmail.com>
2026-04-29 15:06:33 +0200

    fix: styling and correct numbers for studentcourseprogresscard
    Co-authored-by: kevinlun-git <Kevin.lundkvist1@gmail.com>

--- src/features/course/components/student-course-progress-card.tsx
+++ src/features/course/components/student-course-progress-card.tsx
@@ -13,37 +13,45 @@ export function StudentCourseProgressCard({
 
   return (
     <Card variant={"raised"} className="gap-2 py-3">
-      <CardHeader className="text-theme-text font-theme-heading gap-0 text-2xl font-semibold">
-        Progress
+      <CardHeader className="gap-0">
+        <div className="flex items-baseline justify-between">
+          <div className="text-theme-text font-theme-heading text-2xl font-semibold">
+            Progress
+          </div>
+          <div className="text-theme-primary">
+            {courseInfo.unlockedModuleCount}/{courseInfo.totalModuleCount}{" "}
+            Modules unlocked
+          </div>
+        </div>
       </CardHeader>
       <CardContent>
-        <div className="flex w-full items-center">
+        <div className="flex w-full items-center gap-2">
+          <span className="font-theme-heading text-theme-primary w-25 shrink-0 text-left text-sm">
+            Passed modules
+          </span>
           <div className="bg-theme-subtle h-3 flex-1 rounded-full">
             <div
-              className="bg-theme-primary text-theme-primary h-full rounded-full"
+              className="bg-theme-primary h-full rounded-full"
               style={{ width: `${Math.round(progress * 100)}%` }}
             />
           </div>
-          <span className="font-theme-heading w-9 shrink-0 text-right text-sm">
-            {Math.round(progress * 100)}%
-          </span>
           <span className="font-theme-heading text-theme-primary w-9 shrink-0 text-right text-sm">
-            {courseInfo.passedModuleCount}/{courseInfo.totalModuleCount}
+            {Math.round(progress * 100)}%
           </span>
         </div>
 
         <div className="flex w-full items-center gap-2">
+          <span className="font-theme-heading text-theme-secondary w-25 shrink-0 text-left text-sm">
+            Passed levels
+          </span>
           <div className="bg-theme-subtle h-3 flex-1 rounded-full">
             <div
-              className="bg-theme-secondary text-theme-secondary h-full rounded-full"
+              className="bg-theme-secondary h-full rounded-full"
               style={{ width: `${Math.round(mastery * 100)}%` }}
             />
           </div>
-          <span className="font-theme-heading w-9 shrink-0 text-right text-sm">
-            {Math.round(mastery * 100)}%
-          </span>
           <span className="font-theme-heading text-theme-secondary w-9 shrink-0 text-right text-sm">
-            {courseInfo.passedLevelCount}/{courseInfo.totalLevelCount}
+            {Math.round(mastery * 100)}%
           </span>
         </div>
       </CardContent>
