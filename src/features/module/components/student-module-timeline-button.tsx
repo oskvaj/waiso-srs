e29902d0461a9bb1d288fea9ea8337755eca5689
oskvaj <oskarvajlok@gmail.com>
2026-04-30 13:48:02 +0200

    feat: uppcomming reviews card
    Co-authored-by: kevinlun-git <Kevin.lundkvist1@gmail.com>

---
+++ src/features/module/components/student-module-timeline-button.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import Link from "next/link";
+
+export function StudentReviewScheduleButton({ id }: { id: string }) {
+  return (
+    <div>
+      <Link href={`${id}/review-schedule`}>
+        <Card
+          variant="raised"
+          className="text-theme-text group hover:bg-theme-primary/15 h-full justify-center p-6 text-xl font-bold"
+        >
+          <div>Upcomming Reviews</div>
+        </Card>
+      </Link>
+    </div>
+  );
+}
