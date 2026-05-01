3276bb5632b68994b58b664a06fb7c9a68738271
oskvaj <oskarvajlok@gmail.com>
2026-05-01 13:26:43 +0200

    feat: student review button now links to the review page
    Co-authored-by: kevinlun-git <Kevin.lundkvist1@gmail.com>

--- src/features/module/components/student-review-button.tsx
+++ src/features/module/components/student-review-button.tsx
@@ -2,6 +2,7 @@
 
 import { Card } from "@/components/ui/card";
 import { api } from "@/trpc/react";
+import Link from "next/link";
 
 export function StudentReviewButton({ courseIds }: { courseIds: string[] }) {
   const { data: reviewsDue } = api.course.getReviewsDue.useQuery({
@@ -9,30 +10,32 @@ export function StudentReviewButton({ courseIds }: { courseIds: string[] }) {
   });
 
   return (
-    <Card
-      variant="raised"
-      className={`group h-full p-6 transition-colors ${
-        reviewsDue && reviewsDue.totalForAllCoursesSent > 0
-          ? "bg-theme-action/25 hover:bg-theme-action/35 text-theme-action"
-          : "text-theme-text"
-      }`}
-    >
-      <div className="flex items-baseline justify-between">
-        <span className="font-theme-heading text-xl font-semibold">
-          Reviews
-        </span>
-        <span className="font-theme-heading text-xl">
-          {reviewsDue ? (
-            reviewsDue.totalForAllCoursesSent > 0 ? (
-              <div>{reviewsDue.totalForAllCoursesSent}</div>
+    <Link href={`/reviews?courses=${courseIds.join(",")}`}>
+      <Card
+        variant="raised"
+        className={`group h-full p-6 transition-colors ${
+          reviewsDue && reviewsDue.totalForAllCoursesSent > 0
+            ? "bg-theme-action/25 hover:bg-theme-action/35 text-theme-action"
+            : "text-theme-text"
+        }`}
+      >
+        <div className="flex items-baseline justify-between">
+          <span className="font-theme-heading text-xl font-semibold">
+            Reviews
+          </span>
+          <span className="font-theme-heading text-xl">
+            {reviewsDue ? (
+              reviewsDue.totalForAllCoursesSent > 0 ? (
+                <div>{reviewsDue.totalForAllCoursesSent}</div>
+              ) : (
+                "None, good job!"
+              )
             ) : (
-              "None, good job!"
-            )
-          ) : (
-            "Loading"
-          )}
-        </span>
-      </div>
-    </Card>
+              "Loading"
+            )}
+          </span>
+        </div>
+      </Card>
+    </Link>
   );
 }
