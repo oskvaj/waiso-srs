9505cdfe2b499b6472cf472e241983468752f221
kevinlun-git <kevin.lundkvist1@gmail.com>
2026-05-02 16:26:17 +0200

    feat: PWA, fix tiptap version mess
    Co-authored-by: oskvaj <oskarvajlok@gmail.com>

--- next.config.js
+++ next.config.js
@@ -3,8 +3,15 @@
  * for Docker builds.
  */
 import "./src/env.js";
+import withPWA from "next-pwa";
+
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default config;
+export default pwaConfig(config);
